afc617993df1af4a448eaebd15cfa13760b6e98f
Shobhit Chittora <chittorashobhit@gmail.com>
2019-11-18 15:30:08 +0530

    render child tree and add props util

--- src/utils/Renderer/render.js
+++ src/utils/Renderer/render.js
@@ -7,27 +7,64 @@ const setInnerText = function (domNode, text) {
   return domNode
 }
 
+const propsMap = {
+  className: 'class',
+  id: 'id'
+}
+
+const addProps = function (props, domElement) {
+  if (props) {
+    const propsArr = Object.keys(props)
+    if (propsArr.length) {
+      propsArr.forEach(prop => {
+        if (prop in propsMap) {
+          domElement.setAttribute(propsMap[prop], props[prop])
+        }
+      })
+    }
+  }
+}
+
+const renderChildren = function (element, children) {
+  let domElement = createDomNode(element.type)
+
+  addProps(element.props, domElement)
+
+  if (typeof children === 'string') {
+    setInnerText(domElement, children)
+  } else {
+    children.forEach(child => {
+      domElement.appendChild(renderChildren(child, child.props.children))
+    });
+  }
+  return domElement
+}
+
 /**
  * 
- * @param {DomNode} root 
+ * @param {DomNode} mountNode 
  * @param {ReactElement} element 
  */
-const render = function (root, element) {
+const render = function (mountNode, element) {
 
-  if (!root || !(root instanceof window.HTMLElement)) {
+  if (!mountNode || !(mountNode instanceof window.HTMLElement)) {
     return null
   }
 
   if (!element) {
-    return root
+    return mountNode
   }
 
-  let domElement = createDomNode(element.type)
-  setInnerText(domElement, element.props.children)
-  
-  root.appendChild(domElement)
-  
-  return root;
+  let domElement = null
+  if (element.props.children) {
+    domElement = renderChildren(element, element.props.children)
+  }
+
+  if (domElement) {
+    mountNode.appendChild(domElement)
+  }
+
+  return mountNode;
 }
 
 export { render, createDomNode, setInnerText }
--- src/utils/Renderer/render.test.js
+++ src/utils/Renderer/render.test.js
@@ -11,7 +11,7 @@ describe('render', function () {
     global.window = window
     global.document = window.document
   })
-  
+
   it('createDomNode should create DOM node of type', function () {
     let type = 'div';
     let domNode = createDomNode(type, document)
@@ -40,12 +40,37 @@ describe('render', function () {
 
   it('should render a text node', function () {
     const element = createElement('p', {}, 'Hello world!')
-    const root = document.querySelector("div")
+    const mountNode = document.querySelector("div")
 
-    const dom = render(root, element)
+    const dom = render(mountNode, element)
 
     expect(dom.children.length).toEqual(1)
     expect(document.querySelectorAll("p").length).toEqual(1)
     expect(dom.firstChild.tagName).toEqual("P")
+  })
+
+  it('should convert and add props to child', function () {
+    const element = createElement('p', { id: 'p-id', className: 'p-class' }, 'Helloww!')
+    const mountNode = document.querySelector("div")
+
+    const dom = render(mountNode, element)
+
+    expect(dom.children.length).toEqual(1)
+    expect(dom.firstChild.tagName).toEqual("P")
+    expect(dom.firstChild.getAttribute('id')).toEqual("p-id")
+    expect(dom.firstChild.getAttribute('class')).toEqual("p-class")
+  })
+
+  it('should render multiple child nodes', function () {
+    const children = [createElement('p', {}, 'Child1'), createElement('a', {}, 'Child2')]
+    const root = createElement('div', { className: 'app' }, ...children)
+    const mountNode = document.querySelector("div")
+
+    const dom = render(mountNode, root)
+
+    expect(dom.children.length).toEqual(1)
+    expect(dom.firstChild.getAttribute('class')).toEqual('app')
+    expect(dom.firstChild.childNodes[0].tagName).toEqual('P')
+    expect(dom.firstChild.childNodes[1].tagName).toEqual('A')
   })
 })
